e29d8df67d6acda18c0fc2238fc5f1929f7726d3
KindPlayer2 <seanpatricktighe@gmail.com>
2023-02-22 10:01:24 +0000

    lab 4 star map

---
+++ java/src/ie/tudublin/audio1.java
@@ -0,0 +1,53 @@
+package ie.tudublin;
+
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import processing.core.PApplet;
+
+public class audio1 extends PApplet
+{
+
+    Minim minim;
+    AudioInput ai;
+    AudioPlayer ap;
+    AudioBuffer ab;
+
+	public void settings()
+	{
+		size(500, 500);
+	}
+
+    int frameSize = 1024;
+
+	public void setup() {
+		colorMode(HSB);
+        background(0);
+
+        minim = new Minim(this);
+
+        ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
+        ab = ai.mix;
+
+        smooth();
+		
+	}
+
+	
+	public void draw()
+	{	
+		background(0);
+        stroke(255);
+
+        float half = height / 2;
+
+        for(int i = 0; i < ab.size(); i++)
+        {
+            line(i, half , i, half + ab.get(i) * half);
+            circle(250, 100, ab.get(i) * half);
+        }
+     
+
+	}
+}
